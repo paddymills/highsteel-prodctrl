29a68f1c83d119c6a2472a67336fb2829f892cee
paddymills <paddymills@pm.me>
2022-08-01 11:00:51 -0400

    add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,26 @@
 
+// #![warn(missing_docs)]
+
+//! what run production control
+//! 
+//! # Production Control's Automation
+//! 
+//! ## Feature Flags
+//! 
+//! There are [feature flags] for some modules to reduce code compilation
+//! where the features are not needed. By default, only the api is enabled.
+//! 
+//! - `full`: Enables all features
+//! - `api`: Enables the internal business logic api
+//! - `async`: Enables the [tokio] runtime and async dependencies
+//! - `db`: Enables the mssql integration
+//! - `xl`: Enables the excel data contectors
+//! 
+//! [feature flags]: https://doc.rust-lang.org/cargo/reference/features.html#the-features-section
+//! [tokio]: https://tokio.rs
+//! [tokio docs]: https://docs.rs/tokio/latest/tokio/
+//! 
+
 #[cfg(feature = "async")]
 #[macro_use] extern crate async_trait;
 
@@ -11,7 +33,7 @@ pub use crate::core::*; // must use crate::core to resolve ambiguity
 pub mod config;
 pub mod ui;
 
-#[cfg(feature="api")]
+#[cfg(feature = "api")]
 pub mod api;
 
 #[cfg(feature="db")]
