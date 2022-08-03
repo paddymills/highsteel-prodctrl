878399889e7de284f8576038fbc88035f4a4e701
paddymills <paddymills@pm.me>
2022-08-03 13:54:23 -0400

    add LB command

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -5,12 +5,12 @@ use clap::{Parser, Subcommand};
 use prodctrl::JobShipment;
 use prodctrl::db::bom;
 
-// TODO: completions
+// TODO: completions https://docs.rs/clap_complete/latest/clap_complete/index.html
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
 struct Args {
     #[clap(subcommand)]
-    command: Commands,
+    command: Option<Commands>,
 
     #[clap(flatten)]
     verbose: clap_verbosity_flag::Verbosity,
@@ -25,14 +25,23 @@ enum Commands {
         #[clap(short, long)]
         /// Generate xml files for plate parts
         xml: bool,
-
         #[clap(short, long)]
         /// Secondary parts only
         secondary: bool,
-
         #[clap(short, long)]
         /// Plate parts only
         plate: bool,
+    },
+    /// Lateral bracing listing
+    Lb {
+        job_ship: JobShipment,
+
+        #[clap(short, long)]
+        /// Filter parts of this shape
+        shape: String,
+        #[clap(short, long)]
+        /// Filter parts on this drawing
+        drawing: String,
     }
 }
 
@@ -42,15 +51,24 @@ async fn main() -> Result<()> {
     println!("{:#?}", args);
 
     use Commands::*;
-    match &args.command {
-        Bom { job_ship, .. } => {
-            let pool = bom::build_pool().await;
-            let parts = bom::init_bom( pool, &job_ship.job, job_ship.ship.parse()? )
-                .await?
-                .into_iter();
-
-            parts.for_each( |part| println!("{:#?}", part) );
+    if let Some(cmd) = &args.command {
+        match cmd {
+            Bom { job_ship, .. } => {
+                let pool = bom::build_pool().await;
+                let parts = bom::init_bom( pool, &job_ship.job, job_ship.ship.parse()? )
+                    .await?
+                    .into_iter();
+    
+                // TODO: filters
+    
+                parts.for_each( |part| println!("{:#?}", part) );
+            },
+            Lb { .. } => {
+                // TODO: implement this from examples
+            }
         }
+    } else {
+        println!("No args given. Default to menu.")
     }
 
     Ok(())
