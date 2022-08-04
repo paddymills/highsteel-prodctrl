1fcb480c35da2676c61de60ff4f917a32231f9e8
paddymills <paddymills@pm.me>
2022-08-04 14:19:53 -0400

    doc DbClient

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -14,12 +14,19 @@ pub use config::*;
 pub mod prelude {
     use bb8::Pool;
     use bb8_tiberius::ConnectionManager;
-    use tiberius::Client;
-    use tokio_util::compat::Compat;
-    use tokio::net::TcpStream;
 
     /// Convenience export of database Pool type
     pub type DbPool = Pool<ConnectionManager>;
-    /// Convenience export of Client type 
-    pub type DbClient = Client<Compat<TcpStream>>;
+
+    /// Convenience export of Client type
+    /// 
+    /// equivalent to
+    /// ```
+    /// use tiberius::Client;
+    /// use tokio_util::compat::Compat;
+    /// use tokio::net::TcpStream;
+    /// 
+    /// type DbClient = Client<Compat<TcpStream>>;
+    /// ```
+    pub type DbClient = bb8_tiberius::rt::Client;
 }
