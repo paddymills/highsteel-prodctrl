7eb7eb5c95718b2ce59a0af30c099a1bfc5f0f4c
paddymills <paddymills@pm.me>
2022-08-15 14:53:26 -0400

    expand docs

--- src/fs/mod.rs
+++ src/fs/mod.rs
@@ -7,9 +7,13 @@ use std::path::Path;
 
 /// Check if file is is am empty text file.
 /// 
-/// This is done by checking the file's metadata, figuring a 0kb text file
-/// is an empty file. Getting the [std::fs::metadata] can fail, so in the case
-/// of failure, this will return `false`.
+/// This is done by checking the file's metadata, figuring a `0kb` text file
+/// is an empty file. Getting the [`metadata`] can fail, so in the case
+/// of failure, this will return `false`. Considering that getting [`metadata`]
+/// should only fail due to permissions or a non-existant path, this shouldn't
+/// be an issue
+/// 
+/// [`metadata`]: std::fs::metadata
 pub fn is_empty_file<P>(filepath: P) -> bool
     where P: AsRef<Path>
 {
