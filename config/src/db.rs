e2539d22aa15b302d89bebd2d6039e0c036b3c81
paddymills <paddymills@pm.me>
2022-09-20 08:19:22 -0400

    add database and gen

--- config/src/db.rs
+++ config/src/db.rs
@@ -1,15 +1,29 @@
 
-use prodctrl_db::Database;
 use crate::ConfigAssets;
 
 /// Parent config node
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Default, Deserialize, Serialize)]
 pub struct Databases {
     /// Bom database configuration
     pub bom: Database,
 
     /// Sigmanest database configuration
     pub sigmanest: Database
+}
+
+/// Database connection
+#[derive(Debug, Default, Deserialize, Serialize)]
+pub struct Database {
+    /// Server name
+    pub server: String,
+    /// Server instance, if applicable
+    pub instance: Option<String>,
+    /// Database name (optional)
+    pub database: Option<String>,
+    /// User (optional)
+    pub user: Option<String>,
+    /// Password (if applicable)
+    pub password: Option<String>,
 }
 
 impl Databases {
@@ -19,7 +33,16 @@ impl Databases {
             // TODO: compile time asset existance check
             //       to make sure .unwrap() won't panic
             .unwrap()
+            .unwrap()
 
             // TODO: compile time deserialization check
     }
+    
+    pub fn generate() {
+		let cfg = Self::default();
+		let cfg_toml = toml::to_string(&cfg)
+			.expect("failed to serialize config");
+		std::fs::write("assets/config.toml", cfg_toml)
+			.expect("failed to write config data to file");
+	}
 }
