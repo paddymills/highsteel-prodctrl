eb538082731f873c6657db7db11591416b122f16
paddymills <paddymills@pm.me>
2022-09-29 10:07:56 -0400

    fix assets path

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -7,7 +7,7 @@ pub use db::{DbConfig, DbConnParams};
 use rust_embed::RustEmbed;
 
 #[derive(RustEmbed)]
-#[folder = "../assets/"]
+#[folder = "assets/"]
 #[include = "*.toml"]
 /// Embedded config toml files
 struct ConfigAssets;
